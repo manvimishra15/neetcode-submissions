0326708de5486cbf0e499ac010a7781c5227ad3f
manvimishra15 <manvimishra1501@gmail.com>
2026-08-09 02:12:46 +0530

    Add: find-peak-element - submission-0

---
+++ Data Structures & Algorithms/find-peak-element/submission-0.java
@@ -0,0 +1,16 @@
+   class Solution {
+    public int findPeakElement(int[] nums) {
+        int low=0 , high=nums.length-1 , mid=0;
+        
+        while(low<high)
+        {
+            mid= low +(high-low)/2;
+            if(nums[mid+1]<nums[mid])
+            high=mid;
+             
+            else 
+            low=mid+1; 
+        }
+        return low;
+    }
+}
